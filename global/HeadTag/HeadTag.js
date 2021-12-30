fee041ee008928dde9646279d272894c4e9d1994
cutlerwater <cutlerwater2@live.com>
2021-12-30 18:49:27 -0500

    changing logo

--- global/HeadTag/HeadTag.js
+++ global/HeadTag/HeadTag.js
@@ -9,7 +9,7 @@ export const HeadTag = ({ title }) => {
             <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600&display=swap" rel="stylesheet" />
 
             {/* Favicon */}
-            <link rel="shortcut icon" href="/img/favicon.png" type="image/x-icon" />
+            <link rel="shortcut icon" href="/img/logo.jpg" type="image/x-icon" />
 
             {/* Remix Icons CDN */}
             <link href="https://cdn.jsdelivr.net/npm/remixicon@2.5.0/fonts/remixicon.css"
